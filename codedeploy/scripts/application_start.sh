6c1ffb5a2e8bc4e2e392cea971119f0be37a04a0
awskumarsm2405 <awskumarsm2405@gmail.com>
2024-08-28 21:20:57 +0530

    appspec and script

--- codedeploy/scripts/application_start.sh
+++ codedeploy/scripts/application_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+ls -ltr
+pwd
 echo 'run application_start.sh: ';
 nohup java -jar target/account-0.0.1-SNAPSHOT.jar --spring.config.location=/home/application.properties output.log &
 echo 'application started running with id new' 
