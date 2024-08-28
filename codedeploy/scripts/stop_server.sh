d7607fc2db8c1263b80915d8faa5ffbd6e4f7980
awskumarsm2405 <awskumarsm2405@gmail.com>
2024-08-28 18:39:05 +0530

    appspec and script

--- codedeploy/scripts/stop_server.sh
+++ codedeploy/scripts/stop_server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 isExistApp=$(pgrep java)
-if [[ -n  $isExistApp ]]; then
-  echo "Stopping the application."
-  sudo kill -9 $isExistApp
-  echo "The application is stopped."
-else
-  echo "The application is not running."
-fi
+if [ -z "$isExistApp" ]; then    
+ echo "The application is  not running";
+ else
+   echo "Stopping the application.";
+   sudo kill -9 $isExistApp
+   echo "The application is stopped.";     
+     
+ fi
