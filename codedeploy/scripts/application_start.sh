b8d2f0892a4f30bf2f4d4fed66d4a42108f54059
awskumarsm2405 <awskumarsm2405@gmail.com>
2024-08-28 21:07:54 +0530

    update bash script

--- codedeploy/scripts/application_start.sh
+++ codedeploy/scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'run application_start.sh: '; 
-java -jar target/account-0.0.1-SNAPSHOT.jar --spring.config.location=/home/application.properties
+echo 'run application_start.sh: ';
+nohup java -jar target/account-0.0.1-SNAPSHOT.jar --spring.config.location=/home/application.properties output.log &
 echo 'application started running with id' 
 isExistApp=$(pgrep java)
 echo $isExistApp;
