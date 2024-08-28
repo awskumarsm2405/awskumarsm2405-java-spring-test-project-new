d4b53d22f9bd4c0bf22c332e9044a80a73e721ca
awskumarsm2405 <awskumarsm2405@gmail.com>
2024-08-28 21:33:39 +0530

    appspec and script

--- codedeploy/scripts/application_start.sh
+++ codedeploy/scripts/application_start.sh
@@ -2,7 +2,7 @@
 ls -ltr
 pwd
 echo 'run application_start.sh: ';
-nohup java -jar target/account-0.0.1-SNAPSHOT.jar --spring.config.location=/home/application.properties output.log &
+nohup java -jar /var/www/target/account-0.0.1-SNAPSHOT.jar --spring.config.location=/home/application.properties output.log &
 echo 'application started running with id new' 
 isExistApp=$(pgrep java)
 echo $isExistApp;
