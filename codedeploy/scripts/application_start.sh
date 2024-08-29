b705020f0ec89df382e7c9485fd26348f57ffd62
awskumarsm2405 <awskumarsm2405@gmail.com>
2024-08-29 08:31:18 +0530

    appspec and script

--- codedeploy/scripts/application_start.sh
+++ codedeploy/scripts/application_start.sh
@@ -2,5 +2,5 @@
 ls -ltr
 pwd
 echo 'run application_start.sh: ';
-nohup java -jar /var/www/target/account-0.0.1-SNAPSHOT.jar --spring.config.location=/home/application.properties >output.log &
+nohup java -jar /var/www/target/account-0.0.1-SNAPSHOT.jar --spring.config.location=/home/application.properties > /dev/null 2> /dev/null < /dev/null &
 exit 0
